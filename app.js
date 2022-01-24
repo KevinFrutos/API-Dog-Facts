f30d491464fda8b8fdc240b0bfe95dbb3e5cb1b9
KevinFrutos <vinke.14297@gmail.com>
2022-01-24 18:26:28 +0100

    dotenv installed

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-import express from "express"
-import cors from "cors"
-import dogFacts from "./dogfacts.js"
+import express from "express";
+import cors from "cors";
+import {} from "dotenv/config";
+import dogFacts from "./dogfacts.js";
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.get("/", (req, res) => {
-    res.status(200).send("Conectado!")
-})
+	res.status(200).send("Conectado!");
+});
 
 app.get("/all-facts", (req, res) => {
-    res.status(200).json(dogFacts)
-})
+	res.status(200).json(dogFacts);
+});
 
-app.listen(process.env.PORT || 9000)
+app.listen(process.env.PORT || 9000);
