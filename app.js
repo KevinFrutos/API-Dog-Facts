2e55b2ad3fccc0c471005dffabcb7069da716394
KevinFrutos <vinke.14297@gmail.com>
2022-01-24 18:40:49 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-const dogFacts = require("./dogfacts.js");
+const dogFacts = require("./dogfacts");
 
 const app = express();
 
